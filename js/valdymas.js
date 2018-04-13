98e454045115d138af3c31364eaef92bdabbad7b
Ieva Sinkeviciute <ieva1919@gmail.com>
2018-04-13 22:47:14 +0300

    mario valdymas

--- js/valdymas.js
+++ js/valdymas.js
@@ -1,38 +1,115 @@
-var r2d2 = document.getElementById('R2D2');
+var mario = document.getElementById('mario');
+var juodas = document.getElementById('juodas');
 
+mario.pos = {};
+mario.pos.top = 0;
+mario.pos.right = 0;
 
-var topPadetis = 0;
-var rightPadetis = 0;
-
-function judetiAukstyn() {
-    topPadetis -= 104;
-    if(topPadetis <0) {
-        topPadetis = 0;
+mario.judetiAukstyn = function() {
+    mario.pos.top -= 104;
+    if (mario.pos.top < 0) {
+        mario.pos.top = 0;
     }
-    r2d2.style.top = topPadetis + "px";
+    mario.style.top = mario.pos.top + "px";
+    checkCollide()
 }
 
-
-function judetiKaire() {
-    rightPadetis += 104;
-    if(rightPadetis > 832) {
-        rightPadetis = 0;
+mario.judetiKaire = function() {
+    mario.pos.right += 104;
+    if(mario.pos.right > 728) {
+        mario.pos.right = 728;
     }
-    r2d2.style.right = rightPadetis + "px";
-}
+    mario.style.right = mario.pos.right + "px";
+    checkCollide()
+};
 
-function judetiDesine() {
-    rightPadetis -= 104;
-    if(rightPadetis <0) {
-        rightPadetis = 0;
-    }
-    r2d2.style.right = rightPadetis + "px";
-}
 
-function judetiZemyn() {
-    topPadetis += 104;
-    if(topPadetis > 520) {
-        topPadetis = 0;
+mario.judetiDesine = function() {
+    mario.pos.right -= 104;
+    if(mario.pos.right <0) {
+        mario.pos.right = 0;
     }
-    r2d2.style.top = topPadetis + "px";
-}
+    mario.style.right = mario.pos.right + "px";
+    checkCollide()
+};
+
+
+mario.judetiZemyn = function() {
+    mario.pos.top += 104;
+    if(mario.pos.top > 480) {
+        mario.pos.top = 0;
+    }
+    mario.style.top = mario.pos.top + "px";
+    checkCollide()
+};
+
+
+document.onkeyup = function(e) {
+    if(e.keyCode == 37) {
+        mario.judetiKaire();
+    }
+
+    if(e.keyCode == 38) {
+        mario.judetiAukstyn();
+    }
+
+    if(e.keyCode == 39) {
+        mario.judetiDesine();
+    }
+
+    if(e.keyCode == 40) {
+        mario.judetiZemyn();
+    }
+};
+
+var checkCollide = function( ) {
+    $black = $('.black');
+    $mario = $('#mario');
+
+    var mo = $mario.offset();
+
+    $black.each(function() {
+        var bo = $(this).offset();
+        bo.top = Math.floor(bo.top);
+
+        if (mo.top === bo.top && mo.left === bo.left) {
+
+            setTimeout(function() {
+                mario.pos.top = 0;
+                mario.pos.right = 0;
+                mario.style.top = mario.pos.top + "px";
+                mario.style.right = mario.pos.right + "px";
+            }, 200)
+        }
+
+    })
+
+    $mushroom = $('#mushroom');
+
+    var mush = $mushroom.offset();
+    mush.top = Math.floor(mush.top - 1);
+
+
+    if (mush.top === mo.top && mush.left === mo.left) {
+        console.log('ant grybo');
+    }
+
+
+    // // Div 1 data
+    // var d1_offset             = $div1.offset();
+    // var d1_height             = $div1.outerHeight( true );
+    // var d1_width              = $div1.outerWidth( true );
+    // var d1_distance_from_top  = d1_offset.top + d1_height;
+    // var d1_distance_from_left = d1_offset.left + d1_width;
+    //
+    // // Div 2 data
+    // var d2_offset             = $div2.offset();
+    // var d2_height             = $div2.outerHeight( true );
+    // var d2_width              = $div2.outerWidth( true );
+    // var d2_distance_from_top  = d2_offset.top + d2_height;
+    // var d2_distance_from_left = d2_offset.left + d2_width;
+    //
+    // var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
+    //
+    // return ! not_colliding;
+};
